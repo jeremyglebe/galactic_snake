e07f88841aada9a1d6039942fe350ed5470bf543
Jeremy Glebe <jeremyglebe@gmail.com>
2020-12-07 01:57:10 -0600

    file ops web simulations and update for new capacitor

--- src/game.ts
+++ src/game.ts
@@ -3,52 +3,66 @@ import * as Phaser from 'phaser';
 
 import { Plugins } from '@capacitor/core';
 import { SCREEN_WIDE, SCREEN_RATIO } from './constants';
-import { writeFile } from './util/file-ops';
+import { readFile, writeFile } from './util/file-ops';
 
 const config: Phaser.Types.Core.GameConfig = {
-  parent: 'game',
-  type: Phaser.AUTO,
-  backgroundColor: '#000',
-  // Example of sizing based on w/h ratio
-  width: SCREEN_WIDE ? 800 * SCREEN_RATIO : 800,
-  height: SCREEN_WIDE ? 800 : 800 / SCREEN_RATIO,
-  zoom: 1,
-  dom: {
-    createContainer: true
-  },
-  render: {
-    pixelArt: true,
-    roundPixels: true
-  },
-  scale: {
-    mode: Phaser.Scale.FIT
-  }
+    parent: 'game',
+    type: Phaser.AUTO,
+    backgroundColor: '#000',
+    // Example of sizing based on w/h ratio
+    width: SCREEN_WIDE ? 800 * SCREEN_RATIO : 800,
+    height: SCREEN_WIDE ? 800 : 800 / SCREEN_RATIO,
+    zoom: 1,
+    dom: {
+        createContainer: true
+    },
+    render: {
+        pixelArt: true,
+        roundPixels: true
+    },
+    scale: {
+        mode: Phaser.Scale.FIT
+    }
 };
 
 const { StatusBar, SplashScreen } = Plugins;
 
 export class KTGame extends Phaser.Game {
 
-  constructor(config: Phaser.Types.Core.GameConfig) {
+    constructor(config: Phaser.Types.Core.GameConfig) {
 
-    super(config);
+        super(config);
 
-    // Example of scene management
-    // this.scene.add('Boot', BootScene, false);
-    // this.scene.start('Boot');
+        // Example of scene management
+        // this.scene.add('Boot', BootScene, false);
+        // this.scene.start('Boot');
 
-    writeFile(
-      {
-        location: 'documents',
-        filePath: 'wut.txt'
-      },
-      "Hello there"
-    );
+        // File-ops write test
+        writeFile(
+            {
+                location: 'app',
+                filePath: 'kt_fileops_test.txt',
+                webResolve: 'ram'
+            },
+            "Hello there"
+        ).then(
+            () => {
+                // File-ops read test
+                readFile(
+                    {
+                        location: 'app',
+                        filePath: 'kt_fileops_test.txt',
+                        webResolve: 'ram'
+                    }
+                ).then(
+                    text => console.log("File read: ", text)
+                );
+            });
 
-    StatusBar.hide();
-    SplashScreen.hide();
+        StatusBar.hide();
+        SplashScreen.hide();
 
-  }
+    }
 
 }
 
--- src/util/file-ops.ts
+++ src/util/file-ops.ts
@@ -1,19 +1,24 @@
-import { Capacitor, Plugins, FilesystemDirectory } from "@capacitor/core";
-// import * as fs from 'fs';
-// import * as path from 'path';
+import { Capacitor, Plugins, FilesystemDirectory, FilesystemEncoding } from "@capacitor/core";
 const { Filesystem } = Plugins;
-var fs: any = null;
-var path: any = null;
 
-try {
-    fs = window.require('fs');
-    path = window.require('path');
-} catch (e) {
-    console.log("Full filesystem access not available. (Are we on mobile/web?)");
-}
-
+// Upon importing, log the read/write platform in use
 console.log("Read/Write platform: ", Capacitor.platform);
 
+// Importing node libraries when using electron
+var fs: any = null;
+var path: any = null;
+var app: any = null;
+try {
+    // Import node libraries for filesystem
+    fs = window.require('fs');
+    path = window.require('path');
+    app = window.require('electron').remote.app
+} catch (e) {
+    // Log if the full filesystem is not available through node
+    console.log("Full filesystem access not available. (Are we on mobile/web?) ", e);
+}
+
+// Specifier type to determine what file is being read from/written to
 export interface FileSpecifier {
     // relative path to the file from the directory
     filePath: string,
@@ -44,89 +49,159 @@ export interface FileSpecifier {
     webResolve?: string
 }
 
-export async function writeFile(file: FileSpecifier, data: string): Promise<boolean> {
+export async function readFile(file: FileSpecifier): Promise<string> {
+    // If platform is electron, run internal electron read function
     if (Capacitor.platform == "electron") {
-        if (file.location == 'app') {
-            try {
-                await fs.writeFileSync(path.resolve(__dirname, 'appdata', file.filePath), data);
-                return true;
-            } catch {
-                return false;
-            }
-        } else if (file.location == 'documents') {
-            try {
-                await Filesystem.writeFile(
-                    {
-                        directory: FilesystemDirectory.Documents,
-                        path: file.filePath,
-                        data: data
-                    }
-                );
-                return true;
-            } catch {
-                return false;
-            }
-        } else {
-            try {
-                await fs.writeFileSync(path.resolve(file.desktopDataDirectory, file.filePath), data);
-                return true;
-            } catch {
-                return false;
-            }
-        }
-    } else if (Capacitor.platform == 'ios' || Capacitor.platform == 'android') {
-        let directory: any;
-        if (file.location == 'documents') directory = FilesystemDirectory.Documents;
-        else if (file.location == 'data') directory = FilesystemDirectory.Data;
-        else directory = FilesystemDirectory.Data;
-        try {
-            await Filesystem.writeFile(
-                {
-                    directory: directory,
-                    path: file.filePath,
-                    data: data
-                }
-            );
-            return true;
-        } catch {
-            return false;
-        }
+        return await _electronRead(file);
     }
-    // Define behavior on web here
-    // THIS NEEDS IMPLEMENTATION!!!!!!!!
+    // If platform is mobile, run internal mobile read function
+    else if (Capacitor.platform == 'ios' || Capacitor.platform == 'android') {
+        return await _mobileRead(file);
+    }
+    // As a fallback, run the simulated web read
     else {
-        return false;
+        return await _webSimulateRead(file);
     }
 }
-export async function readFile(file: FileSpecifier): Promise<string> {
+
+export async function writeFile(file: FileSpecifier, data: string): Promise<void> {
+    // If platform is electron, run internal electron write function
     if (Capacitor.platform == "electron") {
-        if (file.location == 'app') {
-            return fs.readFileSync(path.resolve(__dirname, 'appdata', file.filePath)).toString();
-        } else if (file.location == 'documents') {
-            return (await Filesystem.readFile(
-                {
-                    directory: FilesystemDirectory.Documents,
-                    path: file.filePath
-                }
-            )).data;
-        } else {
-            return fs.readFileSync(path.resolve(file.desktopDataDirectory, file.filePath)).toString();
-        }
-    } else if (Capacitor.platform == 'ios' || Capacitor.platform == 'android') {
-        let directory: any;
-        if (file.location == 'documents') directory = FilesystemDirectory.Documents;
-        else if (file.location == 'data') directory = FilesystemDirectory.Data;
-        else directory = FilesystemDirectory.Data;
+        await _electronWrite(file, data);
+    }
+    // If platform is mobile, run internal mobile write function
+    else if (Capacitor.platform == 'ios' || Capacitor.platform == 'android') {
+        await _mobileWrite(file, data);
+    }
+    // As a fallback, run the simulated web write
+    else {
+        await _webSimulateWrite(file, data);
+    }
+}
+
+async function _electronRead(file: FileSpecifier): Promise<string> {
+    // Read from folder in app directory "appdata/"
+    if (file.location == 'app') {
+        return fs.readFileSync(path.resolve(app.getAppPath(), 'appdata', file.filePath), { encoding: 'ascii' });
+    }
+    // Read from standard documents folder such as "C:\Users\username\Documents\"
+    else if (file.location == 'documents') {
         return (await Filesystem.readFile(
             {
-                directory: directory,
-                path: file.filePath
+                directory: FilesystemDirectory.Documents,
+                path: file.filePath,
+                encoding: FilesystemEncoding.ASCII
             }
         )).data;
     }
-    // Define behavior on web here
-    // THIS NEEDS IMPLEMENTATION!!!!!!!!
-    else {
-        return null;
+    // Read from a specified data folder on the system
+    else if (file.location == 'data') {
+        return fs.readFileSync(path.resolve(file.desktopDataDirectory, file.filePath), { encoding: 'ascii' });
     }
 }
+
+async function _electronWrite(file: FileSpecifier, data: string): Promise<void> {
+    // Save files to the directory of the application in a folder "appdata/"
+    if (file.location == 'app') {
+        try {
+            fs.accessSync(path.resolve(app.getAppPath(), 'appdata'));
+        } catch {
+            fs.mkdirSync(path.resolve(app.getAppPath(), 'appdata'));
+        }
+        fs.writeFileSync(path.resolve(app.getAppPath(), 'appdata', file.filePath), data, { encoding: 'ascii' });
+    }
+    // Save files to the standard document folder, such as "C:\Users\username\Documents\"
+    else if (file.location == 'documents') {
+        await Filesystem.writeFile(
+            {
+                directory: FilesystemDirectory.Documents,
+                path: file.filePath,
+                data: data,
+                encoding: FilesystemEncoding.ASCII
+            }
+        );
+    }
+    // Save files to a specified data folder on the system
+    else if (file.location == 'data') {
+        try {
+            fs.accessSync(file.desktopDataDirectory);
+        } catch {
+            fs.mkdirSync(file.desktopDataDirectory);
+        }
+        fs.writeFileSync(path.resolve(file.desktopDataDirectory, file.filePath), data, { encoding: 'ascii' });
+    }
+}
+
+async function _mobileRead(file: FileSpecifier): Promise<string> {
+    let directory: any;
+    // Read from the devices documents folder
+    // Android is the system documents folder, ios is the app's documents folder
+    if (file.location == 'documents') directory = FilesystemDirectory.Documents;
+    // 'data' and 'app' are treated the same, read from the mobile devices default data folder
+    else if (file.location == 'data' || file.location == 'app') directory = FilesystemDirectory.Data;
+    return (await Filesystem.readFile(
+        {
+            directory: directory,
+            path: file.filePath,
+            encoding: FilesystemEncoding.ASCII
+        }
+    )).data;
+}
+
+async function _mobileWrite(file: FileSpecifier, data: string): Promise<void> {
+    let directory: any;
+    // Save to the device's default for documents
+    // Android is the system documents folder, ios is the app's documents folder
+    // (Android document files can be accessed by other apps)
+    if (file.location == 'documents') directory = FilesystemDirectory.Documents;
+    // Save to the default data directory of the system. May overlap with documents
+    else if (file.location == 'data' || file.location == 'app') directory = FilesystemDirectory.Data;
+    await Filesystem.writeFile(
+        {
+            directory: directory,
+            path: file.filePath,
+            data: data,
+            encoding: FilesystemEncoding.ASCII
+        }
+    );
+}
+
+async function _webSimulateRead(file: FileSpecifier): Promise<string> {
+    // If using ram to simulate reads, just read it from an object
+    if (file.webResolve == 'ram') {
+        return _fakeStorage[file.filePath];
+    }
+    // If using local storage to simulate reads, make a javascript call
+    else if (file.webResolve == 'local') {
+        return localStorage.getItem(file.filePath);
+    }
+    // Just ignore the read statment
+    else if (file.webResolve == 'pass') {
+        return null;
+    }
+    // If nothing is specified, throw an error
+    else {
+        throw ('Attempted to simulate file ops in browser with no webResolve specified!');
+    }
+}
+
+async function _webSimulateWrite(file: FileSpecifier, data: string): Promise<void> {
+    // If using ram to simulate writes, just store it in an object
+    if (file.webResolve == 'ram') {
+        _fakeStorage[file.filePath] = data;
+    }
+    // If using local storage to simulate writes, make a javascript call
+    else if (file.webResolve == 'local') {
+        localStorage.setItem(file.filePath, data);
+    }
+    // Just ignore the write statment
+    else if (file.webResolve == 'pass') {
+        return;
+    }
+    // If nothing is specified, throw an error
+    else {
+        throw ('Attempted to simulate file ops in browser with no webResolve specified!');
+    }
+}
+
+var _fakeStorage = {};
